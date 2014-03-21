da1eb374e4689517654854711e5016c997f837c1
Acaldas <afonso7caldas@gmail.com>
2014-03-21 10:35:59 +0000

    erros on contacts fixed

--- js/main.js
+++ js/main.js
@@ -6,10 +6,53 @@ $(document).ready(function(){
         $(".team-ball.active").removeClass("active");
         $(this).addClass("active");
     });
-    document.getElementById('photo1').getElementById('team-photo-image').setAttribute('xlink:href','../images/team-photos/Andre.JPG');
+
+    var photo1 = document.getElementById('photo1');
+    if (photo1 != null)
+       change_photos(0);
 
 });
-window.onload=function(){
 
-
+function change_photos(page) {
+    var  photo1.getElementById('team-photo-image').setAttribute('xlink:href','../images/team-photos/Andre.JPG');
 };
+
+var team_photos = [
+    [
+        {
+            "name": "ANDRÉ SILVA",
+            "curso": "Informática",
+            "img": "Andre.JPG"
+        },
+        {
+            "name": "JOÃO FIGUEIREDO",
+            "curso": "Multimédia",
+            "img": "joaofigueiredo.JPG"
+        },
+        {
+            "name": "MARLYN SOUSA",
+            "curso": "Design de Comunicação",
+            "img": "marlynsousa.JPG"
+        },
+        {
+            "name": "Helena Soares",
+            "curso": "Design de Comunicação",
+            "img": "helenasoares2.JPG"
+        }
+    ],
+    [
+
+    ],
+    [
+
+    ],
+    [
+
+    ],
+    [
+
+    ],
+    [
+
+    ]
+]
